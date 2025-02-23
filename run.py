366bd930a5b8e471f82f908b38c5ac37f8e28036
Kristynpos <elbazumwalt@gmail.com>
2025-02-23 13:46:32 +0700

    Add files via upload

---
+++ run.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import os
+
+def envor(x, y):
+	os.environ[x] = os.environ.get(x, y)
+	return os.environ[x]
+
+LEGACY_ADDR = envor("VLOTT_LEGACY_ADDR", "127.0.0.1:7002")
+WORKERS = envor("VLOTT_WORKERS", "1")
+V1_ENABLED = envor("VLOTT_USE_V1", "1")
+
+if __name__ == "__main__":
+	os.system(f"gunicorn -k uvicorn.workers.UvicornWorker app:app --log-level warn -w {WORKERS}")
