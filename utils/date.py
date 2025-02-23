f645525bb2730c305ce38a41e7f5e308b559ccb7
Kristynpos <elbazumwalt@gmail.com>
2025-02-23 13:49:55 +0700

    Create date.py

---
+++ utils/date.py
@@ -0,0 +1,20 @@
+from datetime import date, timedelta
+
+FMT = "%Y-%m-%d"
+
+def monday_before(x: date = date.today()) -> date:
+	return x + timedelta(days=-x.weekday())
+
+def friday_after(x: date = date.today()) -> date:
+	return monday_before(x) + timedelta(days=4)
+
+def next_weekday(x: date) -> date:
+	tomorrow = x + timedelta(days=1)
+	if tomorrow.weekday() >= 5:
+		tomorrow += timedelta(days=7-tomorrow.weekday())
+	return tomorrow
+
+def school_year(x: date = date.today()) -> int:
+	if x.month < 7:
+		return x.year - 1
+	return x.year
